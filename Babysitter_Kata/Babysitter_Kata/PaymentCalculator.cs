64b0e8530447c7fad02d2382ae226c03383e5cd3
Neil Nordhaus <neil@nordhaus.us>
2017-07-27 23:02:02 -0400

    Created project. Added a test, it passes even though it shouldn't (no assert)

---
+++ Babysitter_Kata/Babysitter_Kata/PaymentCalculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Babysitter_Kata
+{
+    public class PaymentCalculator
+    {
+        public decimal GetAmountDue(DateTime start)
+        {
+            return 0m;
+        }
+    }
+}
---
+++ Babysitter_Kata/Babysitter_Kata/PaymentCalculator_Tests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Babysitter_Kata
+{
+    [TestClass]
+    public class PaymentCalculator_Tests
+    {
+        [TestClass]
+        public class GetAmountDue_Should
+        {
+            [TestMethod]
+            public void Throw_Error_For_Start_Before_5()
+            {
+                var sut = new PaymentCalculator();
+
+                var startTime = new DateTime().AddHours(16); // 4 PM
+
+                var actual = sut.GetAmountDue(startTime);
+            }
+        }
+    }
+}
